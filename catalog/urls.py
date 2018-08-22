e404b7b03ceeac6d533168503d83fdeeaf439e6b
kami-jami <kami.jami@gmail.com>
2018-08-22 07:18:33 +0430

    Auth For User

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
+app_name="catalog"
 urlpatterns = [
-    # path('/', views.index),
+    path('', views.Index.as_view(), name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -20,7 +20,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('catalog/', include('catalog.urls', namespace='catalog')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
